da1f1562d776898c8cc8bc1763afe6823d6e8fdf
nicolasbucceri <nicolasbucceri@hotmail.com>
2025-02-01 16:23:21 -0300

    EmialJS

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
-emailjs.init('upc6A1YO3r0aOjNN9'); // Usar solo la Public Key aquí
+// Inicializar EmailJS con tu public key
+emailjs.init('upc6A1YO3r0aOjNN9'); // Usa tu propia clave pública
 
+// Obtener el formulario y agregar el evento de envío
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+  e.preventDefault(); // Evita el comportamiento por defecto del formulario
 
-  // Usamos emailjs.sendForm para enviar el formulario directamente a EmailJS
+  // Usar emailjs.sendForm para enviar el formulario
   emailjs.sendForm('service_wgqca7a', 'template_5exil2t', this)
-      .then(function(response) {
-          console.log('Correo enviado con éxito:', response);
-          alert('¡Mensaje enviado correctamente!');
-      }, function(error) {
-          console.log('Error al enviar el correo:', error);
-          alert('Hubo un problema al enviar el mensaje.');
-      });
+    .then(function(response) {
+      // Si el correo se envió correctamente
+      console.log('Correo enviado con éxito:', response);
+      alert('¡Mensaje enviado correctamente!');
+    }, function(error) {
+      // Si hubo un error al enviar el correo
+      console.log('Error al enviar el correo:', error);
+      alert('Hubo un problema al enviar el mensaje.');
+    });
 });
