ad1603ad190a1f03aa3b529813485bcdfb52ac6e
nicolasbucceri <nicolasbucceri@hotmail.com>
2025-02-01 13:41:25 -0300

    Render Colocado

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middleware para procesar datos del formulario
 app.use(express.json());
