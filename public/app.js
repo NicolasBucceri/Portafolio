8b29ae7bb9be94d4d1cc2dabf7e2ef206691878b
nicolasbucceri <nicolasbucceri@hotmail.com>
2025-02-06 07:48:39 -0300

    EmialJS

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('botonFormulario');
 
-document.getElementById('form')
+document.getElementById('contactForm')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
